93f1be7e0ea8ff55b61676676e65d9e63de34416
Uddashya Kumar <125728691+uddashya@users.noreply.github.com>
2024-07-24 00:39:50 +0800

    Add files via upload

--- import_nifty200.py
+++ import_nifty200.py
@@ -1,10 +1,8 @@
-import os
-import pandas as pd
-import time
+import yfinance as yf
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
-
-
+import pandas as pd
+# InfluxDB settings
 token = 'MlUmh8wWKDAcBiZbBjvJPguGKxoON1ZRqFjVQ_zh8M3NHLcr53WnxbfXX_ByD0Wm7S8G0kb622gj2EX25MzFCA=='
 org = "Portfolio_Opt"
 url = "http://localhost:8080"
@@ -13,84 +11,35 @@ bucket = "Nifty50"
 client = InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
-
-folder_path = '/Volumes/rog-phone5/NF_200/Nifty200_1h'
 nifty50_symbols = [
-    "ADANIENT",  # Adani Enterprises Ltd.
-    "ADANIPORTS",  # Adani Ports and SEZ Ltd.
-    "APOLLOHOSP",  # Apollo Hospitals Enterprise Ltd.
-    "ASIANPAINT",  # Asian Paints Ltd.
-    "AXISBANK",  # Axis Bank Ltd.
-    "BAJAJ-AUTO",  # Bajaj Auto Ltd.
-    "BAJFINANCE",  # Bajaj Finance Ltd.
-    "BAJAJFINSV",  # Bajaj Finserv Ltd.
-    "BHARTIARTL",  # Bharti Airtel Ltd.
-    "BPCL",  # Bharat Petroleum Corporation Ltd.
-    "BRITANNIA",  # Britannia Industries Ltd.
-    "CIPLA",  # Cipla Ltd.
-    "COALINDIA",  # Coal India Ltd.
-    "DIVISLAB",  # Divi's Laboratories Ltd.
-    "DRREDDY",  # Dr. Reddy's Laboratories Ltd.
-    "EICHERMOT",  # Eicher Motors Ltd.
-    "GRASIM",  # Grasim Industries Ltd.
-    "HCLTECH",  # HCL Technologies Ltd.
-    "HDFCBANK",  # HDFC Bank Ltd.
-    "HDFCLIFE",  # HDFC Life Insurance Company Ltd.
-    "HEROMOTOCO",  # Hero MotoCorp Ltd.
-    "HINDALCO",  # Hindalco Industries Ltd.
-    "HINDUNILVR",  # Hindustan Unilever Ltd.
-    "HDFC",  # Housing Development Finance Corporation Ltd.
-    "ICICIBANK",  # ICICI Bank Ltd.
-    "ITC",  # ITC Ltd.
-    "IOC",  # Indian Oil Corporation Ltd.
-    "INDUSINDBK",  # IndusInd Bank Ltd.
-    "INFY",  # Infosys Ltd.
-    "JSWSTEEL",  # JSW Steel Ltd.
-    "KOTAKBANK",  # Kotak Mahindra Bank Ltd.
-    "LT",  # Larsen & Toubro Ltd.
-    "M&M",  # Mahindra & Mahindra Ltd.
-    "MARUTI",  # Maruti Suzuki India Ltd.
-    "NESTLEIND",  # Nestle India Ltd.
-    "NTPC",  # NTPC Ltd.
-    "ONGC",  # Oil and Natural Gas Corporation Ltd.
-    "POWERGRID",  # Power Grid Corporation of India Ltd.
-    "RELIANCE",  # Reliance Industries Ltd.
-    "SBILIFE",  # SBI Life Insurance Company Ltd.
-    "SHREECEM",  # Shree Cement Ltd.
-    "SBIN",  # State Bank of India
-    "SUNPHARMA",  # Sun Pharmaceutical Industries Ltd.
-    "TCS",  # Tata Consultancy Services Ltd.
-    "TATACONSUM",  # Tata Consumer Products Ltd.
-    "TATAMOTORS",  # Tata Motors Ltd.
-    "TATASTEEL",  # Tata Steel Ltd.
-    "TECHM",  # Tech Mahindra Ltd.
-    "TITAN",  # Titan Company Ltd.
-    "ULTRACEMCO"  # UltraTech Cement Ltd.
+    "ADANIENT.NS", "ADANIPORTS.NS", "APOLLOHOSP.NS", "ASIANPAINT.NS",
+    "AXISBANK.NS", "BAJAJ-AUTO.NS", "BAJFINANCE.NS", "BAJAJFINSV.NS",
+    "BHARTIARTL.NS", "BPCL.NS", "BRITANNIA.NS", "CIPLA.NS", "COALINDIA.NS",
+    "DIVISLAB.NS", "DRREDDY.NS", "EICHERMOT.NS", "GRASIM.NS", "HCLTECH.NS",
+    "HDFCBANK.NS", "HDFCLIFE.NS", "HEROMOTOCO.NS", "HINDALCO.NS",
+    "HINDUNILVR.NS", "HDFCBANK.NS", "ICICIBANK.NS", "ITC.NS", "IOC.NS",
+    "INDUSINDBK.NS", "INFY.NS", "JSWSTEEL.NS", "KOTAKBANK.NS", "LT.NS",
+    "M&M.NS", "MARUTI.NS", "NESTLEIND.NS", "NTPC.NS", "ONGC.NS",
+    "POWERGRID.NS", "RELIANCE.NS", "SBILIFE.NS", "SHREECEM.NS", "SBIN.NS",
+    "SUNPHARMA.NS", "TCS.NS", "TATACONSUM.NS", "TATAMOTORS.NS",
+    "TATASTEEL.NS", "TECHM.NS", "TITAN.NS", "ULTRACEMCO.NS"
 ]
 
+# Fetch and write data
+for symbol in nifty50_symbols:
+    data = yf.download(symbol, start="2017-01-01", end=pd.Timestamp.today().strftime('%Y-%m-%d')  , progress=False)
+    for index, row in data.iterrows():
+        point = (
+            Point("market_data")
+            .tag("symbol", symbol)
+            .field("open", float(row['Open']))
+            .field("high", float(row['High']))
+            .field("low", float(row['Low']))
+            .field("close", float(row['Close']))
+            .field("volume", int(row['Volume']))
+            .time(index, WritePrecision.NS)
+        )
+        write_api.write(bucket=bucket, org=org, record=point)
+        print(f"Data for {symbol} on {index.date()} written to InfluxDB.", end="\r")
 
-
-for filename in os.listdir(folder_path):
-    if filename.endswith('.csv'):
-        file_path = os.path.join(folder_path, filename)
-        
-        data = pd.read_csv(file_path, encoding='ISO-8859-1')  # Or try 'cp1252' if 'ISO-8859-1' doesn't work
-        # print(data)
-        
-        for index, row in data.iterrows():
-            if row["symbol"] in nifty50_symbols:
-                print(f"Data from {filename}  at {index} written to InfluxDB.", end="\r")
-                # Create a point with the data
-                point = (
-                    Point("market_data")
-                    .tag("symbol", row['symbol'])
-                    .field("open", float(row['open']))
-                    .field("high", float(row['high']))
-                    .field("low", float(row['low']))
-                    .field("close", float(row['close']))
-                    .field("volume", int(row['volume']))
-                    .time(pd.to_datetime(row['datetime']), WritePrecision.NS)
-                )
-                # Write the point to the InfluxDB
-                write_api.write(bucket=bucket, org=org, record=point)
-
+print("All data written to InfluxDB.")
